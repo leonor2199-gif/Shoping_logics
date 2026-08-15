5ba3b95d89501dc1f81407b92305be7fe4465b2a
leonor2199-gif <leonor2199@gmail.com>
2026-08-14 20:14:47 -0600

    Update Login.jsx

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -35,7 +35,7 @@ function Login() {
       setLoading(true);
 
       const response = await fetch(
-        "http://localhost:5000/api/auth/user/login",
+        `${import.meta.env.VITE_API_URL}/auth/user/login`,
         {
           method: "POST",
 
